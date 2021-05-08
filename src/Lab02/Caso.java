890138b4a9935f6bc428c13e01a505437928ad81
OscarString <Ochilo@unsa.edu.pe>
2021-05-08 18:41:18 -0500

    add esquema

---
+++ src/Lab02/Caso.java
@@ -0,0 +1,15 @@
+package Lab02;
+
+public class Caso {
+	String observacion;
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+	
+
+}
--- src/Lab02/Enfermedad.java
+++ src/Lab02/Enfermedad.java
@@ -1,5 +1,41 @@
 package Lab02;
 
 public class Enfermedad {
+	String codEnfer;
+	String nombEnfer;
+	String tipoEnfer;
+	String sintomas;
+	String medicacion;
+	public String getCodEnfer() {
+		return codEnfer;
+	}
+	public void setCodEnfer(String codEnfer) {
+		this.codEnfer = codEnfer;
+	}
+	public String getNombEnfer() {
+		return nombEnfer;
+	}
+	public void setNombEnfer(String nombEnfer) {
+		this.nombEnfer = nombEnfer;
+	}
+	public String getTipoEnfer() {
+		return tipoEnfer;
+	}
+	public void setTipoEnfer(String tipoEnfer) {
+		this.tipoEnfer = tipoEnfer;
+	}
+	public String getSintomas() {
+		return sintomas;
+	}
+	public void setSintomas(String sintomas) {
+		this.sintomas = sintomas;
+	}
+	public String getMedicacion() {
+		return medicacion;
+	}
+	public void setMedicacion(String medicacion) {
+		this.medicacion = medicacion;
+	}
+	
 
 }
