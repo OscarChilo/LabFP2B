dde0238e5c45658e8b4675cee82b3e2a0a16e919
inando4 <torresluisperez@hotmail.com>
2021-05-08 19:27:34 -0500

    Update Paciente.java
    modificadores de acceso

--- src/Lab02/Paciente.java
+++ src/Lab02/Paciente.java
@@ -2,48 +2,63 @@ package Lab02;
 import java.util.*;
 
 public class Paciente {
-	String dni;
-	String nombres;
-	String apellidos;
-	Date fechNac;
-	String nroCel;
-	String nroCelFam;
+	private String dni;
+	private String nombres;
+	private String apellidos;
+	private Date fechNac;
+	private String nroCel;
+	private String nroCelFam;
+	
+	
+	
 	public String getDni() {
 		return dni;
 	}
+	
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
+	
 	public String getNombres() {
 		return nombres;
 	}
+	
 	public void setNombres(String nombres) {
 		this.nombres = nombres;
 	}
+	
 	public String getApellidos() {
 		return apellidos;
 	}
+	
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+	
 	public Date getFechNac() {
 		return fechNac;
 	}
+	
 	public void setFechNac(Date fechNac) {
 		this.fechNac = fechNac;
 	}
+	
 	public String getNroCel() {
 		return nroCel;
 	}
+	
 	public void setNroCel(String nroCel) {
 		this.nroCel = nroCel;
 	}
+	
 	public String getNroCelFam() {
 		return nroCelFam;
 	}
+	
 	public void setNroCelFam(String nroCelFam) {
 		this.nroCelFam = nroCelFam;
 	}
+	
 	
 
 }
