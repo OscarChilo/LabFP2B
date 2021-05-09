1c1d2d4aa598c1309c13d959db19b5725132a80f
inando4 <torresluisperez@hotmail.com>
2021-05-08 19:15:11 -0500

    Update Hospital.java
    sdfgsdf

--- src/Lab02/Hospital.java
+++ src/Lab02/Hospital.java
@@ -7,6 +7,7 @@ public class Hospital {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		short nroMenu1, nroMenu2;
+		
 		Scanner scan=new Scanner (System.in);
 		
 		
@@ -18,41 +19,49 @@ public class Hospital {
 		nroMenu1=scan.nextShort();
 		switch(nroMenu1){
 		
-		case 1:System.out.println("** PACIENTES **\r\n" + 
-				"1. Crear un paciente\r\n" + 
-				"2. Ver pacientes\r\n" + 
-				"3. Actualizar datos de un paciente\r\n" + 
-				"4. Eliminar un paciente\r\n" + 
-				"5. Regresar menu principal:0");
-		nroMenu2=scan.nextShort();
-		switch(nroMenu2){
-		case 1:System.out.println("** CREAR NUEVO PACIENTE **");break;
-		case 2:System.out.println("** LISTA DE PACIENTES **");break;
-		case 3:System.out.println("** ACTUALIZAR PACIENTE **");break;
-		case 4:System.out.println("** ELIMINAR PACIENTE **");break;
-		case 0:Hospital.main (null);break;
-		}
+			case 1:
+					System.out.println("** PACIENTES **\r\n" + 
+					"1. Crear un paciente\r\n" + 
+					"2. Ver pacientes\r\n" + 
+					"3. Actualizar datos de un paciente\r\n" + 
+					"4. Eliminar un paciente\r\n" + 
+					"5. Regresar menu principal:0");
+			
+					nroMenu2=scan.nextShort();
+					
+					switch(nroMenu2){
+						case 1:System.out.println("** CREAR NUEVO PACIENTE **");break;
+						case 2:System.out.println("** LISTA DE PACIENTES **");break;
+						case 3:System.out.println("** ACTUALIZAR PACIENTE **");break;
+						case 4:System.out.println("** ELIMINAR PACIENTE **");break;
+						case 0:Hospital.main (null);break;
+					}
 		
-		break;
-		case 2:System.out.println("** ENFERMEDADES **\r\n" + 
-				"1. Registrar enfermedad\r\n" + 
-				"2. Ver enfermeddades\r\n" + 
-				"3. Actualizar datos de una enfermedad\r\n" + 
-				"4. Eliminar una enfermedad\r\n" + 
-				"5. Regresar menu principal:0");
-		nroMenu2=scan.nextShort();
-		break;
-		case 3:System.out.println("** CASOS **\r\n" + 
-				"1. Registrar un caso\r\n" + 
-				"2. Ver casoa\r\n" + 
-				"3. Actualizar un caso\r\n" + 
-				"4. Eliminar un caso\r\n" + 
-				"5. Regresar menu principal:0");
-		nroMenu2=scan.nextShort();
-		case 4:System.out.println("** REPORTE FINAL **");
-		break;
-		default:System.out.println("ingrese correctamente la opcion");
-		break;
+					break;
+			case 2:
+					System.out.println("** ENFERMEDADES **\r\n" + 
+							"1. Registrar enfermedad\r\n" + 
+							"2. Ver enfermeddades\r\n" + 
+							"3. Actualizar datos de una enfermedad\r\n" + 
+							"4. Eliminar una enfermedad\r\n" + 
+							"5. Regresar menu principal:0");
+					nroMenu2=scan.nextShort();
+					break;
+			case 3:
+					System.out.println("** CASOS **\r\n" + 
+							"1. Registrar un caso\r\n" + 
+							"2. Ver casoa\r\n" + 
+							"3. Actualizar un caso\r\n" + 
+							"4. Eliminar un caso\r\n" + 
+							"5. Regresar menu principal:0");
+					nroMenu2=scan.nextShort();
+		
+			case 4:
+					System.out.println("** REPORTE FINAL **");
+					break;
+					
+			default:System.out.println("ingrese correctamente la opcion");
+					break;
 			
 		}
 
